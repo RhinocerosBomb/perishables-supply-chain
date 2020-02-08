b8502765aae7a5d7c7d4ff50de79bcd3737cf12a
RhinocerosBomb <rhinocerosbomb@gmail.com>
2020-02-08 05:16:21 -0500

    Fixed mock data

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,15 +2,16 @@ const SupplyChainTracker = artifacts.require("SupplyChainTracker");
 const fs = require('fs');
 
 module.exports = async function(deployer, network, accounts) {
-  deployer.deploy(SupplyChainTracker).then(async() => {
-      const fileContent = await require("../build/contracts/SupplyChainTracker.json");
-      const contractConfiguration = {
-        contract: fileContent,
-        contractAddress: SupplyChainTracker.address,
-        owner: accounts[0]
-    };
+  deployer.deploy(SupplyChainTracker)
+  // .then(async() => {
+  //     const fileContent = await require("../build/contracts/SupplyChainTracker.json");
+  //     const contractConfiguration = {
+  //       contract: fileContent,
+  //       contractAddress: SupplyChainTracker.address,
+  //       owner: accounts[0]
+  //   };
     
-    // Copy contract ABI, contract address, and owner address to the client folder
-    fs.writeFileSync('client/src/contractConfigurations.json', JSON.stringify(contractConfiguration), { flag: 'w' });
-  });
+  //   // Copy contract ABI, contract address, and owner address to the client folder
+  //   fs.writeFileSync('client/src/contractConfigurations.json', JSON.stringify(contractConfiguration), { flag: 'w' });
+  // });
 };
